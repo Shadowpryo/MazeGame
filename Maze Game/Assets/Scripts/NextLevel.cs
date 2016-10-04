d99c756a07ef7c66b0dedaee7e673a70dc9ac338
Shadow Pryo <Shadow_pryo>
2016-10-04 17:57:51 -0400

    Game Done! J/K but it's basic

---
+++ Maze Game/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered Trigger Event");
+        SceneManager.LoadScene(2);
+    }
+}
